84c65f8437b15803f552da8a20915d3e2ac64c24
mavhoka <mavhoka@gmail.com>
2025-03-23 23:35:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
